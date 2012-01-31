737f46062131d5819faf81488905810392bb539f
iwillwen <willwengunn@gmail.com>
2012-01-31 22:12:19 +0800

    a

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-require(__dirname + '/controllers/')()
+var cluster = require('cluster');
+if (cluster.isMaster) {
+  for (var i = 0; i < 1; i++) {
+    var worker = cluster.fork();
+  }
+} else {
+    require(__dirname + '/controllers/')();
+}
+
+cluster.on('death', function(worker) {
+  cluster.fork();
+});
